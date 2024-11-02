150606bec0b63e2d8bbf084ed2c95034c8041d5e
mszahan <mszahan1@gmail.com>
2024-11-03 00:10:53 +0600

    added about us templates

--- loopers/urls.py
+++ loopers/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from loopers import views
 
 urlpatterns = [
-    path('', views.home_page, name='homepage')
+    path('', views.home_page, name='homepage'),
+    path('about-loopers-it/', views.about_us, name='about_us')
 ]
--- loopers/views.py
+++ loopers/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def home_page(request):
     return render(request, 'loopers/home_page.html', {})
+
+def about_us(request):
+    return render(request, 'loopers/about_us.html', {'active':'about'})
