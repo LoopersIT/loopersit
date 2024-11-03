3d33c08529a1c28d47fd3b192e81f9525b16f092
mszahan <mszahan1@gmail.com>
2024-11-03 22:04:18 +0600

    added career detail page template

--- career/urls.py
+++ career/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.career_list, name='career_list')
+    path('', views.career_list, name='career_list'),
+    path('career-detail/', views.career_detail, name='career_detail')
 ]
--- career/views.py
+++ career/views.py
@@ -4,3 +4,5 @@ from django.shortcuts import render
 def career_list(request):
     return render(request, 'career/career_list.html', {'active':'career'})
 
+def career_detail(request):
+    return render(request, 'career/career_detail.html', {'active': 'career'})
