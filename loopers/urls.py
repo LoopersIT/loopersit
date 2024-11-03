83c991863ad7e7cce4bb11f29e58b2ddaf9819de
mszahan <mszahan1@gmail.com>
2024-11-03 11:52:27 +0600

    added contact  us template

--- loopers/urls.py
+++ loopers/urls.py
@@ -3,5 +3,6 @@ from loopers import views
 
 urlpatterns = [
     path('', views.home_page, name='homepage'),
-    path('about-loopers-it/', views.about_us, name='about_us')
+    path('about-loopers-it/', views.about_us, name='about_us'),
+    path('contact-us/', views.contact_us, name='contact_us')
 ]
--- loopers/views.py
+++ loopers/views.py
@@ -7,3 +7,6 @@ def home_page(request):
 
 def about_us(request):
     return render(request, 'loopers/about_us.html', {'active':'about'})
+
+def contact_us(request):
+    return render(request, 'loopers/contact_us.html', {'active': 'contact'})
