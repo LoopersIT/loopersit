94ad74cf22a567c8a0aba530a2ee6b95ef3ec50a
mszahan <mszahan1@gmail.com>
2024-11-03 23:24:46 +0600

    added blog detail page template

--- blog/views.py
+++ blog/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 
 def blog_list(request):
     return render(request, 'blog/list.html', {'active':'blog'})
+
+
+def blog_detail(request):
+    return render(request, 'blog/detail.html', {'active':'blog'})
