e45cae2a1bcfc8b89bc5980bf927a22dede84f43
mszahan <mszahan1@gmail.com>
2024-11-03 18:45:16 +0600

    created new app career

--- config/urls.py
+++ config/urls.py
@@ -6,6 +6,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('loopers.urls')),
+    path('career/', include('career.urls')),
     path('blog/', include('blog.urls'))
 ]
 
