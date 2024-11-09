5da881f7c1cd052a490b5ff1127618d6d89b831b
mszahan <mszahan1@gmail.com>
2024-11-09 11:08:00 +0600

    added job and application in admin panel

--- career/admin.py
+++ career/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Job, Application
 
-# Register your models here.
+
+
+@admin.register(Job)
+class JobAdmin(admin.ModelAdmin):
+    list_display = ['title', 'deadline']
+    prepopulated_fields = {'slug': ('title',)}
+
+
+
+@admin.register(Application)
+class ApplicaitonAdmin(admin.ModelAdmin):
+    list_display = ['role', 'name', 'email', 'mobile_number']
+    list_filter = ['role']
+
