24a50ec84d7ed5752c9e2ed0d90c058cbe13bed1
mszahan <mszahan1@gmail.com>
2024-11-09 10:38:50 +0600

    created job and application model

--- career/models.py
+++ career/models.py
@@ -1,3 +1,56 @@
 from django.db import models
+from ckeditor_uploader.fields import RichTextUploadingField
+from .validators import validate_file_size
 
-# Create your models here.
+
+JOB_TYPE_CHOICES = (
+    ('full_time', 'Full-time'),
+    ('part_time', 'Part-time'),
+
+)
+
+JOB_MODE_CHOICES = (
+    ('on_site', 'On-site'),
+    ('remote', 'Remote'),
+    ('hybrid', 'Hybrid'),
+
+)
+
+
+class Job(models.Model):
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)
+    job_type = models.CharField(max_length=20, choices=JOB_TYPE_CHOICES)
+    job_mode = models.CharField(max_length=20, choices=JOB_MODE_CHOICES)
+    short_info = models.TextField()
+    deadline = models.DateField(auto_now=False, auto_now_add=False)
+    detail = RichTextUploadingField(blank=True, null=True)
+    order = models.PositiveIntegerField(default=1)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ['order', 'deadline']
+        indexes = [models.Index(fields=['order'])]
+   
+    def __str__(self):
+        return self.title
+
+
+class Application(models.Model):
+    role = models.CharField(max_length=250, choices=[], blank=False)
+    name = models.CharField(max_length=250)
+    email = models.EmailField( max_length=100)
+    mobile_number = models.CharField(max_length=20)
+    salary_expectation = models.CharField(max_length=10)
+    cv = models.FileField(upload_to='cvs/%Y/%m', validators=[validate_file_size])
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created', ]
+        indexes = [models.Index(fields=['-created'])]
+   
+    def __str__(self):
+        return self.name
+
+
+
