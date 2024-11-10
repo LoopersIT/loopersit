26974ea795e1916c2a73c980889f8eb870d52fa9
mszahan <mszahan1@gmail.com>
2024-11-10 14:31:08 +0600

    added member model and fixed navbar spacing issue

--- about/models.py
+++ about/models.py
@@ -13,3 +13,24 @@ class FAQ(models.Model):
         ordering = ['order']
 
 
+MEMBERS_ROLE_CHOICES = (
+    ('leader', 'Leader'),
+    ('member', 'Member'),
+)
+
+class Member(models.Model):
+    role = models.CharField(max_length=10, choices=MEMBERS_ROLE_CHOICES )
+    name = models.CharField(max_length=150)
+    image = models.ImageField(upload_to= 'members/%Y/%m',blank=True, null=True)
+    designation = models.CharField(max_length=250, blank=True)
+    linkedin = models.URLField(blank=True, help_text='please use https:// before link')
+    github = models.URLField(blank=True, help_text='please use https:// before link')
+    facebook = models.URLField(blank=True, help_text='please use https:// before link')
+    order = models.PositiveIntegerField(default=1)
+
+    class Meta:
+        ordering = ['order',]
+        indexes = [models.Index(fields=['order'])]
+   
+    def __str__(self):
+        return self.name
