f5aba084d325ff0f37221e29566f0c2ba2dfb524
sojib-ali <sojib.ali@hotmail.com>
2025-10-29 15:19:07 +0600

    configured the project for render deployment

--- config/settings/pro.py
+++ config/settings/pro.py
@@ -1,25 +1,14 @@
 from .base import *
-import json
-from django.core.exceptions import ImproperlyConfigured
+import os
+from dotenv import load_dotenv
+from urllib.parse import urlparse, parse_qsl
+
+load_dotenv()
+
+tmpPostgres = urlparse(os.getenv("DATABASE_URL"))
 
 
-with open('secrets.json') as f:
-    secrets = json.load(f)
-
-
-def get_secret(setting, secrets=secrets):
-    '''Get the secret variable or return explicit exception.'''
-    try:
-        return secrets[setting]
-    except KeyError:
-        error_msg = 'Set the {0} environment variable'.format(setting)
-        raise ImproperlyConfigured(error_msg)
-
-
-
-
-SECRET_KEY = (get_secret('SECRET_KEY'), 'django-insecure-w1q2!*29ja2_oaj=&!-stm8a5q7!3ymq!knx78h@)%v1-y80fl')
-
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = False
 
@@ -33,18 +22,30 @@ ALLOWED_HOSTS = ['157.230.230.195', 'loopersit.com', 'www.loopersit.com']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'loopdb',
-        'USER': 'loopdbuser',
-        'PASSWORD': get_secret('PGRESS_PASSWORD'),
-        'HOST': 'localhost',
-        'PORT': '5432'
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': tmpPostgres.path.replace('/', ''),
+        'USER': tmpPostgres.username,
+        'PASSWORD': tmpPostgres.password,
+        'HOST': tmpPostgres.hostname,
+        'PORT': 5432,
+        'OPTIONS': dict(parse_qsl(tmpPostgres.query)),
     }
 }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'loopdb',
+#         'USER': 'loopdbuser',
+#         'PASSWORD': os.getenv('PGRESS_PASSWORD'),
+#         'HOST': 'localhost',
+#         'PORT': '5432'
+#     }
+# }
+
 EMAIL_HOST = 'smtp.gmail.com' 
 EMAIL_HOST_USER = 'szahan4@gmail.com' 
-EMAIL_HOST_PASSWORD = get_secret('MAIL_PASSWORD')
+EMAIL_HOST_PASSWORD = os.getenv('MAIL_PASSWORD')
 EMAIL_PORT = 587 
 EMAIL_USE_TLS = True
 
